ca79f7ac8d400d12bd89d975254e37a5971c037f
lyes <lyes.aici95@yahoo.fr>
2017-12-16 16:08:52 +0100

    code refactor

--- core/src/com/team5/seawar/cam/ZoomCam.java
+++ core/src/com/team5/seawar/cam/ZoomCam.java
@@ -15,13 +15,11 @@ public class ZoomCam extends PlayScreenCam{
 
     public void init(PlayScreen playScreen) {
         super.init(playScreen);
-        zoom = 1;
+        this.zoom = 1;
     }
 
     public void update(float dt) {
         playScreen.getCam().zoom += (zoom - playScreen.getCam().zoom) * 0.02f;
-        playScreen.getCam().position.add((playScreen.hexWidth/2 + playScreen.getPosition().x * playScreen.hexWidth*.75f - playScreen.getPosition().x)*0.03f,
-                (playScreen.hexHeight/2 + playScreen.getPosition().y * playScreen.hexHeight - playScreen.getCam().position.y)*0.03f,
-                0);
+        playScreen.getCam().position.add((playScreen.hexWidth/2 + playScreen.getPosition().x * playScreen.hexWidth*.75f - playScreen.getPosition().x)*0.03f, (playScreen.hexHeight/2 + playScreen.getPosition().y * playScreen.hexHeight - playScreen.getCam().position.y)*0.03f,0);
     }
 }
