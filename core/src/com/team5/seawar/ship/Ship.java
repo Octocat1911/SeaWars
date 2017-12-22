6c487f609c4fcbd08a160ac95c0735cce53e3661
Octocat1911 <lyes.aici95@yahoo.fr>
2017-12-22 14:57:45 +0100

    Update Ship.java

--- core/src/com/team5/seawar/ship/Ship.java
+++ core/src/com/team5/seawar/ship/Ship.java
@@ -3,9 +3,6 @@ package com.team5.seawar.ship;
 import com.badlogic.gdx.graphics.Texture;
 import com.team5.seawar.objects.Element;
 
-public class Ship extends Element {
-    @Override
-    public Texture getTexture() {
-        return null;
-    }
+public class Ship{
+  
 }
