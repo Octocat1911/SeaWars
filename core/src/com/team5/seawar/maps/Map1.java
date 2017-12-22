443779a96d46ea9578819a3fb6165854ae8cca23
Anivial <33802820+Anivial@users.noreply.github.com>
2017-12-22 14:29:27 +0100

    Integration (#18)
    * Map (#11)
    
    * new Branch
    
    * change variables
    
    * Update packages names
    
    * Mise en place des classes et méthodes liées à Map, Element.
    Gestion des Assets.
    Modifications dans Game afin de permettre le lancement d'une PlayScreen
    
    * Ajout des Textures de base.
    
    * Meilleure gestion des assets
    
    * Amelioration de la texture hexTerre
    
    * Dépolution du code d'affichage enlevant SCALE
    
    * Refractor Game to GameApp
    
    * Main menu added + assets (#12)
    
    * Delete Game.java
    
    * Update MenuScreen.java
    
    * Input (#13)
    
    * Ajout de la classe InputHandler qui est non valide pour le moment.
    Elle sera à reprendre.
    
    * Nouveau test
    
    * test ajout p
    
    * Creation de la classe InputHandler et Touche.
    
    * Classe Touche fixed
    
    * Delete Game.java
    
    * Première intégration globale
    
    * Ajout de la texture de pointeur
    
    * Amelioration de la cam (ydown false)
    
    * Amelioration de l'InputHandler
    
    * Mise en place de la gestion d'un click sur un sprite
    
    * Debut implémentation cam
    
    * Meilleur implémentation des cameras
    
    * MenuScreen and InputHandling changed + Added dynamic sprite actions
    
    * Cleared menuscreen
    
    * Cleared menuscreen
    
    * code refactor
    
    * code refactor
    
    * code refactor, working code only please
    
    * code refactor, PLEASE !! Only push working code please
    
    * code refactor, PLEASE !! Only push working code please
    
    * Added Exit button action
    
    * Integration (#14)
    
    * Première intégration globale
    
    * Ajout de la texture de pointeur
    
    * Amelioration de la cam (ydown false)
    
    * Amelioration de l'InputHandler
    
    * Mise en place de la gestion d'un click sur un sprite
    
    * Debut implémentation cam
    
    * MenuScreen and InputHandling changed + Added dynamic sprite actions
    
    * Cleared menuscreen
    
    * Cleared menuscreen
    
    * code refactor
    
    * code refactor
    
    * code refactor, working code only please
    
    * code refactor, PLEASE !! Only push working code please
    
    * code refactor, PLEASE !! Only push working code please
    
    * Added Exit button action
    
    * Dépolution du code
    
    * Project refactoring
    
    * Project refactoring
    
    * Delete PlayScreenCam.java
    
    * Update GlobalCam.java
    
    * Update PlayScreen.java
    
    * Delete hexPointeur.png
    
    * Delete play2.png
    
    * Mise en place des Elements sous forme d'Action2DSprite
    
    * Added Actions
    
    * Mise en place de Case

--- core/src/com/team5/seawar/maps/Map1.java
+++ core/src/com/team5/seawar/maps/Map1.java
@@ -1,9 +1,8 @@
 package com.team5.seawar.maps;
 
 
-import com.team5.seawar.objects.Water;
-import com.team5.seawar.objects.Lighthouse;
-import com.team5.seawar.objects.Dirt;
+import com.team5.seawar.objects.Case;
+import com.team5.seawar.objects.Element;
 
 public class Map1 extends Map {
 
@@ -11,19 +10,19 @@ public class Map1 extends Map {
         super(13, 11);
         for (int i=0; i<colonne; i++){
             for (int j=0; j<ligne; j++){
-                tab[i][j]= Water.getInstance();
+                tab[i][j]= new Case(Element.Type.WATER, i, j);
             }
         }
-        tab[0][0]= new Lighthouse();
-        tab[12][10]= new Lighthouse();
-        tab[6][5]= new Lighthouse();
+        tab[0][0]= new Case(Element.Type.LIGHTHOUSE, 0, 0);
+        tab[12][10]= new Case(Element.Type.LIGHTHOUSE, 12, 10);
+        tab[6][5]= new Case(Element.Type.LIGHTHOUSE, 6, 5);
 
-        tab[3][6]= Dirt.getInstance();
-        tab[3][7]= Dirt.getInstance();
-        tab[4][7]= Dirt.getInstance();
-        tab[8][3]= Dirt.getInstance();
-        tab[9][2]= Dirt.getInstance();
-        tab[9][3]= Dirt.getInstance();
+        tab[3][6]= new Case(Element.Type.DIRT, 3,6);
+        tab[3][7]= new Case(Element.Type.DIRT, 3, 7);
+        tab[4][7]= new Case(Element.Type.DIRT, 4, 7);
+        tab[8][3]= new Case(Element.Type.DIRT, 8, 3);
+        tab[9][2]= new Case(Element.Type.DIRT, 9, 2);
+        tab[9][3]= new Case(Element.Type.DIRT, 9, 3);
 
     }
 
---
+++ core/src/com/team5/seawar/screens/playstates/ShipSelect.java
@@ -0,0 +1,4 @@
+package com.team5.seawar.screens.playstates;
+
+public class ShipSelect implements State {
+}
