6b804140ada20a1866790b878cc84acd293c06a1
Octocat1911 <lyes.aici95@yahoo.fr>
2017-12-22 14:53:59 +0100

    Player gamemaster (#19)
    * Work Done
    
    * Delete Dirt.java
    
    * Delete Lighthouse.java
    
    * Delete Water.java
    
    * Delete Player.java

--- core/src/com/team5/seawar/player/Player.java
+++ core/src/com/team5/seawar/player/Player.java
@@ -1,4 +1,33 @@
 package com.team5.seawar.player;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.team5.seawar.ship.Ship;
+
+import java.util.List;
+
 public class Player {
+    private String nickname;
+    private Sprite avatar;
+    private List<Ship> ships;
+
+    public Player(){
+
+    }
+
+    public Ship getShip(int i){
+        return ships.get(i);
+    }
+
+    public int getShipsSize(){
+        return ships.size();
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public Sprite getAvatar() {
+        return avatar;
+    }
+
 }
