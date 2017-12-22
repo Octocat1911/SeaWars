7a1df232031beccf69b053cd7e88f451054f36f3
Baptiste <bneveu@enssat.fr>
2017-12-22 15:32:08 +0100

    Class Amiral, Canon, Fregate and Ship created

--- core/src/com/team5/seawar/ship/Canon.java
+++ core/src/com/team5/seawar/ship/Canon.java
@@ -1,4 +1,45 @@
 package com.team5.seawar.ship;
 
+import com.badlogic.gdx.math.Vector2;
+
 public class Canon {
+    private Vector2 range;
+    private int damage;
+    private int cooldown;
+
+
+    public Canon(Vector2 range, int damage, int cooldown){
+        this.range = range;
+        this.damage = damage;
+        this.cooldown = cooldown;
+    }
+
+    public Canon(){
+
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getCooldown() {
+        return cooldown;
+    }
+
+    public void setCooldown(int cooldown) {
+        this.cooldown = cooldown;
+    }
+
+    public Vector2 getRange() {
+        return range;
+    }
+
+    public void setRange(Vector2 range) {
+        this.range = range;
+    }
+
 }
--- core/src/com/team5/seawar/ship/Ship.java
+++ core/src/com/team5/seawar/ship/Ship.java
@@ -1,4 +1,54 @@
 package com.team5.seawar.ship;
 
 public class Ship {
+    private int lifepoints;
+    private Canon mainCanon;
+    private Canon secondaryCanon;
+    private int currentMovements;
+    private int maxMovements;
+
+    public Ship(int lifepoints, Canon mainCanon, Canon secondaryCanon, int currentMovements, int maxMovements){
+        this.lifepoints = lifepoints;
+        this.mainCanon = mainCanon;
+        this.secondaryCanon = secondaryCanon;
+        this.currentMovements = currentMovements;
+        this.maxMovements = maxMovements;
+    }
+
+    public int getLifepoints() {
+        return lifepoints;
+    }
+
+    public Canon getMainCanon() {
+        return mainCanon;
+    }
+
+    public void setMainCanon(Canon mainCanon) {
+        this.mainCanon = mainCanon;
+    }
+
+    public Canon getSecondaryCanon() {
+        return secondaryCanon;
+    }
+
+    public void setSecondaryCanon(Canon secondaryCanon) {
+        this.secondaryCanon = secondaryCanon;
+    }
+
+    public int getCurrentMovements() {
+        return currentMovements;
+    }
+
+    public void setCurrentMovements(int currentMovements) {
+        this.currentMovements = currentMovements;
+    }
+
+    public int getMaxMovements() {
+        return maxMovements;
+    }
+
+    public void setMaxMovements(int maxMovements) {
+        this.maxMovements = maxMovements;
+    }
+
 }
